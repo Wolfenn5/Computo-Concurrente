a876b85cfe0e26c68e018d6773149fed6be071de
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-03 21:39:36 -0600

    correcion programa pares_impares_var_cond.cpp  ya funciona

--- pares_impares_var_cond.cpp
+++ pares_impares_var_cond.cpp
@@ -17,25 +17,29 @@ std::condition_variable variable_condicion;
 
 int suma_pares=0;
 int suma_impares=0;
-int numero=0;
-int terminado_pares=0, terminado_impares=0; // banderas para la impresion
-bool par=false, impar= false; // banderas para la impresion
+int numero_par=0;
+int numero_impar=0;
+bool par=false, impar= false, terminado_pares=0, terminado_impares=0; // banderas para la impresion
 
 
 void calculo_pares()
 {
     for (int i=2; i<=100; i=i+2) // sumar desde 2 hasta 100 solo numeros pares
     {
-        std::lock_guard<std::mutex> lock (mutex); // sirve para liberar de forma "semi-automatica" el mutex en vez de usar lock y unlock
-        numero= i;
-        suma_pares= suma_pares + numero;
-        par= true;
+        {
+            std::lock_guard<std::mutex> lock (mutex); // sirve para liberar de forma "semi-automatica" el mutex en vez de usar lock y unlock
+            numero_par= i;
+            suma_pares= suma_pares + numero_par;
+            par= true;
+        }
         variable_condicion.notify_one(); // notificar al hilo de impresion que modificamos algo
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    std::lock_guard<std::mutex> lock(mutex);
-    terminado_pares=true;
-    variable_condicion.notify_one();   
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        terminado_pares=true;
+    }
+    variable_condicion.notify_one(); // notificar al hilo de impresion que modificamos algo
 }
 
 
@@ -44,16 +48,20 @@ void calculo_impares()
 {
     for (int i=1; i<=100; i=i+2) // sumar desde 2 hasta 100 solo numeros impares
     {
-        std::lock_guard<std::mutex> lock (mutex); // sirve para liberar de forma "semi-automatica" el mutex en vez de usar lock y unlock
-        numero= i;
-        suma_impares= suma_impares + numero;
-        impar= true;
+        {
+            std::lock_guard<std::mutex> lock (mutex); // sirve para liberar de forma "semi-automatica" el mutex en vez de usar lock y unlock
+            numero_impar= i;
+            suma_impares= suma_impares + numero_impar;
+            impar= true;
+        }
         variable_condicion.notify_one(); // notificar al hilo de impresion que modificamos algo
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    std::lock_guard<std::mutex> lock(mutex);
-    terminado_impares=true;
-    variable_condicion.notify_one();
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        terminado_impares=true;
+    }
+    variable_condicion.notify_one(); // notificar al hilo de impresion que modificamos algo
 }
 
 
@@ -63,13 +71,11 @@ void imprimir()
     {
         //std::lock_guard<std::mutex> lock (mutex);
         std::unique_lock<std::mutex> lock (mutex); // unique lock sirve para bloquear y desbloquear varios mutex
-        //variable_condicion.wait(lock, [] { return par || impar || terminado_pares && terminado_impares}); // wait se queda esperando y hace que el mutex se libere de forma temporal. sus argumentos son std::lock y luego se dice que va a hacer cuando se despierte el hilo
-          variable_condicion.wait(lock, [] { return par || impar || (terminado_pares && terminado_impares); });
-
-        
+        variable_condicion.wait(lock, [] { return par || impar || (terminado_pares && terminado_impares); }); // wait se queda esperando y hace que el mutex se libere de forma temporal. sus argumentos son std::lock y luego se dice que va a hacer cuando se despierte el hilo
         // ademas una vez que termine de escuchar, se vuelve a bloquear
-
-
+        
+        
+        
         if (par) // si el numero es par se imprime la suma de los pares
         {
             std::cout<<"Numero agregado a la suma de pares"<<" y la suma total es: "<<suma_pares<<"\n";
