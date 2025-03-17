acd0511397a441ade06fe4489b0cb606d8a70a65
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-17 08:33:32 -0600

    hola mundo de cuda

---
+++ hola_mundo_cuda.cu
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* de momento este programa es paralelo */
+
+// se va a trabajar con 9 hilos
+
+__global__ void kernel ()
+{
+    printf("\nHola desde el bloque %d, hilo %d\n", blockIdx.x, threadIdx.x); // blockidx y threadidx son para obtener el bloque y el hilo de ese bloque en eje x (los 2 hilo y bloque)
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    int num_bloques= 3;
+    int num_hilos= 3;
+    printf("\nEjecutando kernel en CUDA \n");
+
+    kernel<<<num_bloques,num_hilos>>>();
+    cudaDeviceSynchronize(); 
+    return 0;
+}
