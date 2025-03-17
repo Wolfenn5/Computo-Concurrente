2643f0a96711e68964c633454d63a27031924fe3
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-17 08:41:51 -0600

    hola mundo version 2 de cuda

---
+++ hola_mundo_dos_cuda.cu
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* de momento este programa es paralelo */
+
+// se va a trabajar con 9 hilos
+
+__global__ void kernel ()
+{
+    printf("\nHola desde el bloque %d, hilo %d\n", blockIdx.x, threadIdx.x); // blockidx y threadidx son para obtener el bloque y el hilo de ese bloque en eje x (los 2 hilo y bloque)
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    int num_bloques;
+    int num_hilos;
+    cudaDeviceProp propiedades;
+    cudaGetDeviceProperties (&propiedades,0);
+
+    num_bloques= propiedades.multiProcessorCount; // numero de multiprocesadores que soporta la GPU (como los bloques)
+
+    num_hilos= propiedades.maxThreadsPerBlock;
+    printf("\nEjecutando kernel en CUDA \n");
+
+    kernel<<<num_bloques,num_hilos>>>();
+    cudaDeviceSynchronize(); 
+    return 0;
+}
