6dd15137cd3de4584c0c6dae08c60d4bab35b8b3
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-17 09:31:53 -0600

    modificacion hola mundos de cuda

--- hola_mundo_cuda.cu
+++ hola_mundo_cuda.cu
@@ -7,6 +7,8 @@
 __global__ void kernel ()
 {
     printf("\nHola desde el bloque %d, hilo %d\n", blockIdx.x, threadIdx.x); // blockidx y threadidx son para obtener el bloque y el hilo de ese bloque en eje x (los 2 hilo y bloque)
+    __syncthreads(); // sincronizar los hilos dentro del mismo bloque
+    printf("\nHola despues de la sincronizacion y soy el bloque %d, hilo%d \n", blockIdx.x, threadIdx.x); 
 }
 
 
--- hola_mundo_dos_cuda.cu
+++ hola_mundo_dos_cuda.cu
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/* de momento este programa es paralelo */
+/* de momento este programa es paralelo. CUDA por naturaleza es asincrono */
 
 // se va a trabajar con 9 hilos
 
 __global__ void kernel ()
 {
     printf("\nHola desde el bloque %d, hilo %d\n", blockIdx.x, threadIdx.x); // blockidx y threadidx son para obtener el bloque y el hilo de ese bloque en eje x (los 2 hilo y bloque)
+    __syncthreads(); // sincronizar los hilos dentro del mismo bloque
+    printf("\nHola despues de la sincronizacion y soy el bloque %d, hilo%d \n", blockIdx.x, threadIdx.x); 
 }
 
 
@@ -24,6 +26,10 @@ int main(int argc, char const *argv[])
     printf("\nEjecutando kernel en CUDA \n");
 
     kernel<<<num_bloques,num_hilos>>>();
-    cudaDeviceSynchronize(); 
+
+
+    cudaDeviceSynchronize(); // es el equivalente a .join en hilos. Es importante utilizarlo para no trabar la GPU
+
+    printf("\nEn total puede ejecutar %d hilos\n", num_bloques*num_hilos); // numero de hilos totales
     return 0;
 }
