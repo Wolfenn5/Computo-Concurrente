ae906da65e7503c3d446e73e2adb47cfeb92b46e
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-21 22:06:48 -0600

    se añadio el ejemplo de dim3 en plantilla

--- plantilla.cpp
+++ plantilla.cpp
@@ -17,6 +17,39 @@
 // totalHilos= 1025* 1025 * 1024= 1,075,840,000;
 
 
+
+
+/* Ejemplo sencillo de dim3 con gridsize y blocksize*/
+
+// #include <stdio.h>
+// #include <cuda_runtime.h>
+
+
+// __global__ void kernel()
+// {
+// int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+// printf("Hilo ID: %d\n", threadId);
+// }
+
+
+
+// int main()
+// {
+// dim3 gridSize(2, 1, 1); //malla de 2 bloques en x, 1 bloque en y, 1 bloque en z
+// // el numero total de bloques en la cuadricula es 2x1x1= 2
+
+// dim3 blockSize(3, 1, 1); //bloques de 3 hilos en x, 1 hilo en y, 1 hilo en z
+// // cada bloque tendra 3x1x1= 3 hilos
+
+
+// // Asi el numero total de hilos en toda la cuadricula va a ser de 2x3= 6
+// kernel<<<gridSize, blockSize>>>();
+// cudaDeviceSynchronize();
+// return 0;
+// }
+
+
+
 // Obtener datos del dispositivo GPU
 cudaDeviceProp propiedades;
 cudaGetDeviceProperties(&propiedades,0);
