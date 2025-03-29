f6dd46010b01269cbd1a6b36009d9bf2c32a9760
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-28 20:38:33 -0600

    se corrigio el script de python para que funcione

--- graficar_tiempos.py
+++ graficar_tiempos.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 # tiempo promedio de ejecuciones
-tiempo_secuencial= [60] # promedio de resultados de ejecucion del algoritmo secuencial
+tiempo_secuencial= 60 # promedio de resultados de ejecucion del algoritmo secuencial
 
 
 # promedio de resultados de ejecucion del algoritmo multihilado 
@@ -17,22 +17,23 @@ tiempos_gpu=[12,9,7,6,5] #poner valores reales
 speedup_gpu= [tiempo_secuencial / tiempo for tiempo in tiempos_gpu]
 print(speedup_gpu)
 
-
+plt.subplot(1,2,1) # 1 fila 2 columnas
 plt.axhline(y=tiempo_secuencial, color= 'r', linestyle='--', label='secuencial') # esta se ocupa para la ejecucion secuencial
-
 plt.plot(hilos_cpu, tiempos_cpu, marker='o', label= 'Multihilo CPU')
-plt.plot(hilos_cpu, tiempos_gpu, marker='s', label= 'Multihilo GPU')
-
-
-plt.title("Grafica de tiempos de ejecucion")
+plt.plot(hilos_gpu, tiempos_gpu, marker='s', label= 'Multihilo GPU')
+plt.title("Tiempos de ejecucion (suma de vectores")
 plt.xlabel('Numero de hilos o tamaño del bloque')
 plt.ylabel('Tiempo de ejecucion promedio (s)')
 plt.grid()
 plt.legend()
-plt.subplot(1,2,2) # de la grafica que se genero esta va a ser la segunda grafica
+
+
+plt.subplot(1,2,2) # 1 fila 2 columnas, 2da grafica 
 plt.plot(hilos_cpu, speedup_cpu, marker='o', label= 'Speedup CPU')
 plt.plot(hilos_gpu, speedup_gpu, marker='s', label= 'Speedup GPU')
 plt.title('Grafica del speedup')
 plt.xlabel('Numero de hilos o tamaño del bloque')
-plt.ylabel('speedup')
+plt.ylabel('Speedup')
+plt.grid()
+plt.legend()
 plt.show()
