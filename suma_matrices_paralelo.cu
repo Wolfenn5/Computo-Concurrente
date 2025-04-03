91f504b3d44cd8dedde92606548c974317a4f8d5
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-04-02 23:11:46 -0600

    se comento un print nadamas en suma matrices paralelo

--- suma_matrices_paralelo.cu
+++ suma_matrices_paralelo.cu
@@ -140,7 +140,7 @@ int main(int argc, char const *argv[])
 
 
     // Imprimir matriz C
-    printf("\nMatriz resultado:\n");
+    // printf("\nMatriz resultado:\n");
     // for (int i=0; i<N; i++) 
     // {
     //     for (int j=0; j<N; j++) 
