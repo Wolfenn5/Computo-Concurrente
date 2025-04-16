578685f0a0a2d043b3799159f3dfff91473f21a4
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-04-16 10:07:41 -0600

    revisar programa de mpi n aleatorios

--- mpi_receiver_n_aleatorios.c
+++ mpi_receiver_n_aleatorios.c
@@ -14,7 +14,7 @@ mpiexec -n 4 ./mpi
 Con el lcluster y la lista de nodos en el /home/rober/lista_nodos.txt
 mpiexec -n 4 --hostfile /home/rober/lista_nodos.txt ./mpi
 
-
+Para este programa incluir -lm para enlazar la biblioteca de math.h
 
 
 Para subir archivos hay que escribir:
@@ -39,15 +39,15 @@ int main(int argc, char **argv)
         for (int i=1; i<size; i++)
         {
             char nombre_host[MPI_MAX_PROCESSOR_NAME];
-            int aleatorio= 1+rand()%10;
-            MPI_Send(&aleatorio,MPI_MAX_PROCESSOR_NAME,MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //donde se va a guardar,tamaño del mensaje, tipo de mensaje, de donde viene, etiqueta, comunicador, ignorar el estado porque en la misma funcion se tienen los datos de cada proceso
-            printf("\nElnviamos el numero %d desde el proceso %d al proceso %d\n",aleatorio,rank,i);
+            aleatorio= 1+rand()%10;
+            MPI_Send(&aleatorio,MPI_MAX_PROCESSOR_NAME,MPI_INT,i,0,MPI_COMM_WORLD); //donde se va a guardar,tamaño del mensaje, tipo de mensaje, de donde viene, etiqueta, comunicador, ignorar el estado porque en la misma funcion se tienen los datos de cada proceso
+            printf("\nEnviamos el numero %d desde el proceso %d al proceso %d\n",aleatorio,rank,i);
         }
         // Una vez que se envia, hay que esperar a que todos tengan su calculo
         for (int i=0; i<size; i++)
         {
             MPI_Recv(&aleatorio,1,MPI_INT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-            printf("\nRecibimos el mensaje con el numero %d con el numero %d",i,aleatorio);
+            printf("\nRecibimos el mensaje del proceso %d con el numero %d\n",i,aleatorio);
         }
         
     }
@@ -55,11 +55,10 @@ int main(int argc, char **argv)
     else
     {
         int aleatorio;
-        char nombre_receptor[MPI_MAX_PROCESSOR_NAME]; // variable donde se guarda el nombre del host con tamaño maximo
         MPI_Recv(&aleatorio,MPI_MAX_PROCESSOR_NAME,MPI_INT,0,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE); //variable que se va a enviar, el tamaño de la variable, tipo caracter de MPI, identificador del proceso, etiqueta del mensaje
-        printf("\nEl proceso %s recibio el mensaje %d\n",rank,aleatorio);
+        printf("\nEl proceso %d recibio el mensaje %d\n",rank,aleatorio);
         int cuadrado=pow(aleatorio,2);
-        MPI_send(&cuadrado,1,MPI_INT,0,0,MPI_COMM_WORLD);
+        MPI_Send(&cuadrado,1,MPI_INT,0,0,MPI_COMM_WORLD);
         printf("\nEnviamos el mensaje %d desde el proceso %d",cuadrado,rank);
     }
 
